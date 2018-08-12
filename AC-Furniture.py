80e146c106dd4dcde72c1de826e1e9a60619ef2d
Joern Rettweiler <spiele@rettweiler.name>
2018-08-12 13:23:21 +0200

    Version 1.0
    First Version

---
+++ AC-Furniture.py
@@ -0,0 +1,151 @@
+# Generates simply 2d rectangular furniture elments
+# 
+# GNU GENERAL PUBLIC LICENSE Version 3
+#
+# by Joern Rettweiler, 2018 august 12
+#
+#
+# Tested with Rhino 5 
+#
+# NO WARRANTY
+#
+# Note: If you want to create a rhino command that you can call via command promt
+# look here: https://developer.rhino3d.com/guides/rhinopython/creating-rhino-commands-using-python/
+
+
+
+import rhinoscriptsyntax as rs
+import Rhino as rhino
+
+
+
+def ac_furniture():
+    
+    
+    
+    #Parent layer 
+    layer_furniture = ("Furniture")
+    layer_furniture_color = [0,0,0]
+    
+    #Enter / Edit Here your furniture layer names EDIT ONLY VALUES IN PARANTHESES,
+    #default :3D geom Color red = [255,0,0]
+    layer_geom2d = ("AC-Geom2d")
+    layer_geom2d_color = [255,0,0]
+    
+    #default :3D geom Colorwhite = [255,255,255]
+    layer_geom3d = ("AC-Geom3d")
+    layer_geom3d_color = [255,255,255]
+    
+    #default : workarea Color blue = [0,255,255]
+    layer_workarea = ("AC-Working_area")
+    layer_workarea_color = [0,255,255]
+    
+    #default : workarea Color blue = [0,0,0]
+    layer_text = ("AC-Text")
+    layer_text_color = [0,0,0]
+    
+    
+    #############################
+    ##FUNCTION DEFINITIONS BEGIN##
+    ##############################
+    
+    
+    
+    
+    def ac_geom2d(width, depth):
+        global geom_text, geom2d, blockname, basepoint
+        
+        planexy = rs.WorldXYPlane()
+        geom2d = rs.AddRectangle(planexy, width, depth)
+        
+        
+        
+        geom2d_centroid = rs.CurveAreaCentroid(geom2d)
+        
+        basepoint = geom2d_centroid[1]
+        #rs.AddPoint((geom2d_centroid))
+        
+        #info text
+        textdottext = (furniture_name + "\n" 
+        + str(width)+ " x " + str(depth) + "\n" 
+        + "newline")
+        
+        
+        #rs.AddPoint(geom2d_centroid[0])
+        geom_text = rs.AddTextDot(textdottext, geom2d_centroid[0])
+        #rhino.text
+        #Generate Blockname
+        blockname = (str(furniture_name) + str(width) + "x" + str(depth))
+        
+    
+    #############################
+    ##FUNCTION DEFINITIONS END###
+    #############################
+    
+    
+    
+    print("hello")
+    
+    #filling cabinet = aktenschrank, shelf=regal
+    
+    furniture_typ = rs.GetInteger("1=filling cabinet 2=Shelf 3=sideboard 4=table, 5=desk", 5, 0, 5 )
+    print furniture_typ
+    
+    furniture_width = rs.GetReal("Width of furniture [cm]", 80, 0)
+    furniture_depth = rs.GetReal("Depth of furniture [cm]", 42, 0)
+    
+    print ("width")
+    print furniture_width
+    print furniture_depth
+    
+    if bool(furniture_typ) != 1 or 2 or 3:
+        
+        furniture_hight = rs.GetReal("Hight of furniture [cm]", 80, 0)
+    else:
+        print("noPenis")
+        furniture_hight = 80
+
+
+    #Add text to textvariable
+    if furniture_typ == 1:
+        furniture_name = "filling cabinet"
+    
+    elif furniture_typ == 2:
+        furniture_name = "Shelf"
+    elif furniture_typ == 3:
+        furniture_name = "Sideboard"
+    elif furniture_typ == 4:
+        furniture_name = "Table"
+    elif furniture_typ == 5:
+        furniture_name = "Desk"
+    #elif furniture_typ == 3:
+    #    furniture_name = "XXX"
+    
+    
+    print furniture_name
+
+
+
+
+
+    #Add layer after previous operation are succesfull
+    rs.AddLayer(layer_furniture, layer_furniture_color)
+    rs.AddLayer(layer_geom2d, layer_geom2d_color, parent=layer_furniture)
+    rs.AddLayer(layer_geom3d, layer_geom3d_color, parent=layer_furniture)
+    rs.AddLayer(layer_workarea, layer_workarea_color, parent=layer_furniture)
+    rs.AddLayer(layer_text, layer_text_color, parent=layer_furniture)
+    
+    
+    
+    rs.CurrentLayer(layer=layer_geom2d)
+    ac_geom2d(furniture_width, furniture_depth)
+    
+    rs.AddBlock([geom_text,geom2d], basepoint, blockname, delete_input=True)
+    rs.InsertBlock(blockname, basepoint)
+    
+
+
+
+
+#call function
+ac_furniture()
